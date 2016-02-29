af894d8347309232bdc4fee3d819440e33735cbf
Brian Oldfield <brian+wiw@oldfield.io>
2016-02-29 07:53:19 -0800

    Minor tweak to wiw-standalone-blog-rolebook

--- cookbooks/wiw-standalone-blog-rolebook/attributes/default.rb
+++ cookbooks/wiw-standalone-blog-rolebook/attributes/default.rb
@@ -3,10 +3,13 @@ include_attribute 'wiw-mysql'
 include_attribute 'wiw-blog'
 include_attribute 'wiw-hhvm'
 
-
-default.wiw_mysql.service_name = 'wiw-blog'
+# wiw-blog settings
 default.wiw_blog.domain = 'blog.wheniwork.com'
 default.wiw_blog.ssl_vault = 'wiw-blog'
+
+# wiw-mysql settings
+default.wiw_mysql.service_name = 'wiw-blog'
 default.wiw_mysql.password_vault = 'mysql'
 default.wiw_mysql.tablename = 'wiw'
 default.wiw_mysql.username = 'wiw'
+default.wiw_mysql.bind_address = '127.0.0.1'
